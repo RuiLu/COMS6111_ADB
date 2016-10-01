86636f4be74d47c4598bb41cd26a82bd53e57439
rz2337 <rz2337@columbia.edu>
2016-10-01 15:56:44 -0400

    Modify the type of function in Rocchio
    That function should return the ArrayList of key words as next input of
    search.

--- Project_1/src/Rocchio.java
+++ Project_1/src/Rocchio.java
@@ -1,8 +1,16 @@
 import java.util.*;
 
 public class Rocchio {
-	public ArrayList<Double> Rocchio_Algorithm() {
-		ArrayList<Double> res = new ArrayList<Double>();
+	public ArrayList<String> Rocchio_Algorithm(
+			Query q, 
+			ArrayList<Document> revelant, 
+			ArrayList<Document> nonrevelant
+			) {
+		double alpha = 1;
+		double beta = 0.75;
+		double gamma = 0.25;
+		
+		ArrayList<String> res = new ArrayList<String>();
 		
 		return res;
 	}
