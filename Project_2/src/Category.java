9bd8ee7de0154ddffa53e709a40f4e12fd09a4e0
rz2337 <rz2337@columbia.edu>
2016-10-25 12:08:26 -0400

    Add coverage and specificity into Category
    Store e-coverage and e-specificity in class Category

--- Project_2/src/Category.java
+++ Project_2/src/Category.java
@@ -5,6 +5,8 @@ public class Category {
 	public String name;
 	public ArrayList<String> query;
 	public ArrayList<Category> subCategory;
+	public Integer ecoverage=0;
+	public Double especificity=1.0;
 	public Category(String n) {
 		name=n;
 		query=new ArrayList<String>();
